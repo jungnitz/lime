7cc0ea46377109d174e57cd9bdf6cf418967a119
jungnitz <git@jungnik.org>
2025-06-03 11:44:29 +0200

    wip

--- rs/src/gp/mod.rs
+++ rs/src/gp/mod.rs
@@ -1,32 +1,81 @@
-pub struct Operand(pub u64);
+mod dsl;
+mod ops;
+mod program;
+mod state;
 
-pub struct LogicFunction;
+use std::fmt::Debug;
+use std::hash::Hash;
 
-pub struct OperandClass {
-    pub name: &'static str,
-    pub members: Vec<Operand>,
-    pub infinite: bool,
+pub type CellIndex = u32;
+
+/// Abstractly describes a Logic-in-Memory architecture.
+pub trait Architecture: Sized + 'static {
+    type CellType: CellType;
+    type Cell: Cell;
+
+    fn outputs(&self) -> &'static [OperandSet<Self>];
+    fn operations(&self) -> &'static [OperationType<Self>];
+
+    fn cell(&self, typ: Self::CellType, idx: CellIndex) -> Self::Cell;
 }
 
-pub struct OperationType {
-    pub inputs: Vec<OperationInput>,
-    pub outputs: OperationOutput,
+pub trait CellType: Copy + Debug + Eq + Hash {
+    type Arch: Architecture;
+
+    /// Number of cells of this type or `None` if infinite amount is available.
+    fn count(self) -> Option<CellIndex>;
 }
 
-pub struct OperationInput {
-    class: OperandClass,
-    output: OperationInputResult,
+pub trait Cell: Copy + Debug + Eq + Hash {
+    type Arch: Architecture;
+
+    fn typ(self) -> <Self::Arch as Architecture>::CellType;
+    fn index(self) -> CellIndex;
 }
 
-pub enum OperationInputResult {
-    Destroyed,
+pub enum OperandType<A: Architecture> {
+    Constant,
+    Cell {
+        typ: A::CellType,
+        inverted: bool,
+        index: Option<CellIndex>,
+    },
+}
+
+pub enum Operand<A: Architecture> {
+    Constant(bool),
+    Cell(A::Cell),
+}
+
+pub enum OperandSet<A: Architecture> {
+    Cross(&'static [OperandType<A>]),
+    Set(&'static [&'static [OperandType<A>]]),
+}
+
+pub struct OperationType<A: Architecture> {
+    pub input: OperandSet<A>,
+    pub input_results: Vec<InputFunction>,
+    pub output: Function,
+}
+
+pub struct Operation<A: Architecture> {
+    pub typ: OperationType<A>,
+    pub operands: Vec<Operand<A>>,
+    pub outputs: Vec<A::Cell>,
+}
+
+pub struct Function {
+    pub inverted: bool,
+    pub gate: Gate,
+}
+
+pub enum Gate {
+    And(Vec<bool>),
+    Maj(Vec<bool>),
+}
+
+pub enum InputFunction {
     Unchanged,
-    Function(LogicFunction),
+    Destroyed,
+    Function(Function),
 }
-
-pub struct OperationOutput {
-    class: OperandClass,
-    function: LogicFunction,
-}
-
-pub struct Operation {}
